a40163ed89ee274ea14c43fa0c147053f9855a72
Gino <ggino3567@gmail.com>
2025-08-28 08:31:35 +0800

    Update : Change Get () [2025.8.28]

--- MyCoreAPI_First/Controllers/TestAddPostController.cs
+++ MyCoreAPI_First/Controllers/TestAddPostController.cs
@@ -9,31 +9,22 @@ namespace MyCoreAPI_First.Controllers
     //基底路徑就是 https://localhost:7090/TestAddPost。
     public class TestAddPostController : ControllerBase
     {
+        /*
         private readonly ILogger<TestAddPostController> _logger;
 
         public TestAddPostController(ILogger<TestAddPostController> logger)
         {
             _logger = logger;
         }
-
-        private static readonly string[] FeelStates = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+         */
 
         [HttpGet("GetTest")] //Controller命名；[HttpGet("N")]就是URL路徑 [HttpGet(Name = "Z")]是給它命名
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = FeelStates[Random.Shared.Next(FeelStates.Length)]
-            })
-            .ToArray();
+            return Ok(new { message = "HelloWorld!" });
         }
 
-        //為她準備一個小數據結構
+        //為Post準備一個小數據結構
         private static Dictionary<string, PostRequest> virtualDb = new Dictionary<string, PostRequest>();
 
         [HttpPost("PostTest")]
